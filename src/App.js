b9fdf6642785af82da6d535eccae14eaf306d617
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-08-04 21:20:22 -0400

    home page file set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+
+import HomePage from './pages/HomePage.js'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <HomePage/>
+
+
+            </div>
+        )
+    }
 }
 
 export default App;
