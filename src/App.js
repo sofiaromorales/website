b1f8313b7b892dd1277945aafa5e0ad79ab02f82
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-09-04 01:21:06 -0400

    analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ReactGA from 'react-ga';
 import React from 'react';
 import './styles/App.css'
 import HomePage from './pages/HomePage.js'
 
+function initializeReactGA() {
+    ReactGA.initialize('UA-147078244-1');
+    ReactGA.pageview('/homepage');
+}
 
 class App extends React.Component {
     render() {
