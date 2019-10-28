f701bbee13fad26b6d7754a89b1ae67c5c454629
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-10-27 22:40:41 -0400

    about me responsive

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -12,14 +12,11 @@ class AboutMe extends Component {
     render() {
         return(
             <Row id='AboutMe' >
-                <Col md={7} span={1}>
-                </Col>
-                <Col md={10} span ={22} style={{
-                    top: '50px',
+                <Col span={1} md={7}/>
+                <Col span ={24} md={10} style={{
                     color:' #515151',
                     fontFamily: "Avenir",
                     fontSize: '20px',
-
                 }}>
                     <p style = {{
                         fontSize: '48px',
@@ -29,23 +26,28 @@ class AboutMe extends Component {
                     }}>
                         Sofia Rodriguez
                     </p>
-                    <p>Hello! I’m a 19 years developer from Caracas, Venezuela.</p>
-                    <p>I create websites, apps (iOS) and design.</p>
-                    <p>I have attended to the Apple Worldwide Developers Confrence three times as an Apple scholar.</p>
-                    <p> My main goal is turn ideas into reality creating beautiful and engaging products. </p>
-                    <p>Interested in working together? Just shoot me a message and let’s see what I can do for you!.</p>
-                    <Row  type="flex" justify="center" style={{
-                        margin: 50
-                    }}>
-                        <Col md={8}>
+                    <Col span={1}/>
+                    <Col span={22}>
+                        <p>Hello! I’m a 19 years developer from Caracas, Venezuela.</p>
+                        <p>I create websites, apps (iOS) and design.</p>
+                        <p>I have attended to the Apple Worldwide Developers Confrence three times as an Apple scholar.</p>
+                        <p> My main goal is turn ideas into reality creating beautiful and engaging products. </p>
+                        <p>Interested in working together? Just shoot me a message and let’s see what I can do for you!.</p>
+                    </Col>
+                        <Col span={6}/>
+                        <Col span={12}>
                             <button type="button" onClick={handleLetsTalkClick} id="talkBtn">
                                 Lets talk!
                             </button>
                         </Col>
-                    </Row>
+                        <Col span={6}/>
+                    <Col span={1}/>
+
+
+
                 </Col>
-                <Col md={7} span={1}>
-                </Col>
+                <Col span={1} md={7}/>
+
 
 
 
