61a2f47ac8a64d5ec8faed5740aac180e3acc42f
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-10-27 20:22:47 -0400

    vlog link set up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import ReactGA from 'react-ga';
 import React from 'react';
+
+import { withRouter, Route } from 'react-router-dom';
+
 import './styles/App.css'
 import HomePage from './pages/HomePage.js'
+import Vlog from './pages/Vlog.js'
 
 ReactGA.initialize('UA-147078244-1');
 
 class App extends React.Component {
     render() {
         return (
-            <div>
 
-                <HomePage/>
-
+            <div className='App'>
+                <Route
+                    exact
+                    path='/'
+                    render={(props) => (
+                        <HomePage/>
+                    )}
+                />
+                
 
 
             </div>
+
         )
     }
 }
---
+++ src/pages/Vlog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Vlog extends Component {
+    render() {
+        return(
+            <div>
+                vlog
+            </div>
+        );
+    }
+}
+
+export default Vlog
