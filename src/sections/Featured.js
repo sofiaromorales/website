af88de2dc9792bc0c38fb095c3700036b843ef43
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-10-27 23:13:27 -0400

    featured responsive 2

--- src/sections/Featured.js
+++ src/sections/Featured.js
@@ -13,12 +13,12 @@ class Featured extends Component {
 
                 <Col md={10} span={20}>
                     <a href='http://elucabista.com/tag/sofia-rodriguez/' target='_blank' rel='noopener noreferrer'>
-                        <img style = {{maxWidth:'100%', padding:25}} src={ucabista} alt='elucabista.com'/>
+                        <img style = {{maxWidth:'90%', padding:25}} src={ucabista} alt='elucabista.com'/>
                     </a>
                 </Col>
                 <Col md={10} span={20}>
                     <a href='https://es.abcufoundation.org/sofia-rodriguez/' target='_blank'>
-                        <img style = {{maxWidth:'100%', padding:25}} src={abcu} alt='elucabista.com'/>
+                        <img style = {{maxWidth:'90%', padding:25}} src={abcu} alt='elucabista.com'/>
                     </a>
                 </Col>
 
