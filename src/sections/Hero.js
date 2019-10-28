a7fbf6ef3de33bcfec19639f7b9fce43854fe574
Sofia Rodriguez <sofiaromorales@gmail.com>
2019-10-27 22:13:58 -0400

    Hero responsive

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -20,46 +20,40 @@ function NavigationBarLinks(props){
 class Hero extends Component {
     render() {
         return(
-            <div id='Hero'>
-                <Row type= 'flex' justify="space-around">
-                    <Col span={24} style={{height:70}}></Col>
-                    <Col span={1}></Col>
-                    <Col md={12} span={20}>
-                        <Title id = 'Title'>
-                            Sofia Rodriguez
-                        </Title>
-                     </Col>
-                     <Col md={8} span={20}>
-                         <ul style={{
-                             listStyleType: 'none',
-                             position: 'absolute',
-                             float: 'right',
-                         }}>
-                             <li><NavigationSocialLinks link = "https://www.instagram.com/sofiaro.m/" target="_blank" title = "Instagram"/></li>
-                             <li><NavigationSocialLinks link = "https://twitter.com/sofiarom_00" target="_blank" title = "Twitter"/></li>
-                             <li><NavigationSocialLinks link = "https://github.com/sofiarm21" target="_blank" title = "Github"/></li>
-                             <li><NavigationSocialLinks link = "mailto:sofiaromorales@gmail.com" title = "Email"/></li>
-                         </ul>
-                      </Col>
-                     <Col md={1}></Col>
+            <Row id='Hero'>
+                <Col span={2}/>
+                <Col span={20}>
+                <Row>
+                        <Col span={24} style={{height:70}}/>
+                        <Col md={12} span={24}>
+                            <Title id = 'Title'>
+                                Sofia Rodriguez
+                            </Title>
+                        </Col>
+                        <Col md={12} span={24}>
+
+                                 <li style={{float:'right'}}><NavigationSocialLinks link = "https://www.instagram.com/sofiaro.m/" target="_blank" title = "Instagram"/></li>
+                                 <li style={{float:'right'}}><NavigationSocialLinks link = "https://twitter.com/sofiarom_00" target="_blank" title = "Twitter"/></li>
+                                 <li style={{float:'right'}}><NavigationSocialLinks link = "https://github.com/sofiarm21" target="_blank" title = "Github"/></li>
+                                 <li style={{float:'right'}}><NavigationSocialLinks link = "mailto:sofiaromorales@gmail.com" title = "Email"/></li>
+
+                        </Col>
                 </Row>
-                <Row type="flex"  align="bottom" style = {{height:620}}>
-                    <Col md={1}></Col>
-                    <Col md={22}>
-                        <Row >
-                            <ul style={{
-                                listStyleType: 'none',
-                            }}>
-                                <li><NavigationBarLinks link = "#AboutMe" title = "About Me"/></li>
+                <Row style={{ height:600}}align="bottom"  type="flex">
+                        <Col md={24}>
+
+                                <li style={{listStyleType: 'none'}}><NavigationBarLinks link = "#AboutMe" title = "About Me"/></li>
                                 <li><NavigationBarLinks link = "#Skills" title = "Skills" /></li>
                                 <li><NavigationBarLinks link = "#Projects" title = "Projects"/></li>
-                            </ul>
-                        </Row>
-                    </Col>
-                    <Col md={1}></Col>
+
+
+                        </Col>
                 </Row>
 
-            </div>
+                </Col>
+                <Col span={2}/>
+
+            </Row>
         );
     }
 }
