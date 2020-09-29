2fb51dfac4d34081c09ac7d965a96064e4715ad2
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-29 00:01:05 -0400

    bootstraped

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { Progress,Row, Col,  Typography } from 'antd';
+import { Row, Col } from 'react-bootstrap'
 import SkillRing from '../components/SkillRing'
+import styled from 'styled-components'
 
 
 import 'react-circular-progressbar/dist/styles.css';
+
+const Icon = styled.img`
+    height:20px;
+    width:auto
+`
 
 
 class Skills extends Component {
@@ -12,9 +18,9 @@ class Skills extends Component {
         return(
 
 
-            <Row id='Skills' type="flex" justify="center" >
+            <Row id='Skills' className='justify-content-center' >
 
-                <Col span={24} md={18}>
+                <Col xs={10}>
 
                     <p style = {{
                         fontSize: '28px',
@@ -23,16 +29,38 @@ class Skills extends Component {
                     }}>
                         Skills
                     </p>
-                    <Row type="flex" justify="space-around" >
-                        <Col md={24}>
-                            <SkillRing skill='Swift' percentage='80'/>
-                            <SkillRing skill='Design' percentage='95'/>
+                    <Row>
+                        <Col xs={2}>
+                            <Icon src='https://user-images.githubusercontent.com/49292858/94510451-ce491780-01e4-11eb-95c0-d4e69d7fcd61.png'/>
+                            Swift
                         </Col>
-                        <Col md={24}>
-                            <SkillRing skill='React' percentage='75'/>
-                            <SkillRing skill='Java' percentage='57'/>
+                        <Col xs={2}>
+                            <Icon src='https://user-images.githubusercontent.com/49292858/94510451-ce491780-01e4-11eb-95c0-d4e69d7fcd61.png'/>
+                            Swift
+                        </Col>
+                        <Col xs={2}>
+                            <Icon src='https://user-images.githubusercontent.com/49292858/94510451-ce491780-01e4-11eb-95c0-d4e69d7fcd61.png'/>
+                            Swift
+                        </Col>
+                        <Col xs={2}>
+                            <Icon src='https://user-images.githubusercontent.com/49292858/94510451-ce491780-01e4-11eb-95c0-d4e69d7fcd61.png'/>
+                            Swift
                         </Col>
                     </Row>
+
+
+
+                    {// <Row type="flex" justify="space-around" >
+                    //     <Col md={24}>
+                    //         <SkillRing skill='Swift' percentage='80'/>
+                    //         <SkillRing skill='Design' percentage='95'/>
+                    //     </Col>
+                    //     <Col md={24}>
+                    //         <SkillRing skill='React' percentage='75'/>
+                    //         <SkillRing skill='Java' percentage='57'/>
+                    //     </Col>
+                    // </Row>
+                }
 
                 </Col>
 
